2342f27737618f5c7248839d6bda40fb5f19001e
Jonathan Kulisic <jonathan.kulisic@maginteractive.se>
2017-05-04 18:05:09 +0100

    Added example .ini file query

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -22,6 +22,7 @@ install() {
 
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
 # of available packages, where PLATFORM is `osx` or `win`
+# http://netstorage.unity3d.com/unity/3829d7f588f3/unity-5.5.2f1-osx.ini
 
 install "MacEditorInstaller/Unity-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
