95d37ba5851021685b90ce364e725db2a52035e4
Jonathan Kulisic <jonathan.kulisic@maginteractive.se>
2017-05-04 19:36:07 +0100

    Added retry to Unity installer download

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -9,7 +9,7 @@ download() {
   url="$BASE_URL/$HASH/$package"
 
   echo "Downloading from $url: "
-  curl -o `basename "$package"` "$url"
+  curl -o `basename "$package"` "$url" —-retry 5
 }
 
 install() {
